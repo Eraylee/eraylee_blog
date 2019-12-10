6f9af2cdcb7a8835310cd55f02ac3194cd93ef1d
eraylee <599023328@qq.com>
2019-12-11 00:30:31 +0800

    首页数据获取

--- pages/model.ts
+++ pages/model.ts
@@ -1,11 +1,25 @@
-import { action } from "easy-peasy";
+/*
+ * @Author: ERAYLEE
+ * @Date: 2019-12-04 17:34:03
+ * @LastEditors: ERAYLEE
+ * @LastEditTime: 2019-12-10 22:19:31
+ */
+import { action, thunk } from "easy-peasy";
 import { IArticleModel } from "./types";
+import { apiGetArticles } from "../api";
+
 const ArticleModel: IArticleModel = {
-  article: {
-    name: "312321"
-  },
+  articles: [],
   setArticles: action((state, payload) => {
-    state.article = payload;
+    state.articles = payload;
+  }),
+  getArticles: thunk(async (actions, payload) => {
+    try {
+      const res = await apiGetArticles(payload);
+      return res.data.data;
+    } catch (error) {
+      console.error(error);
+    }
   })
 };
 export default ArticleModel;
