da991519adf82071d417296513c28c442aaf8f14
eraylee <599023328@qq.com>
2020-01-12 22:46:52 +0800

    新增友链页面

---
+++ pages/Links/style.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: ERAYLEE
+ * @Date: 2020-01-12 21:48:11
+ * @LastEditors  : ERAYLEE
+ * @LastEditTime : 2020-01-12 21:53:45
+ */
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((_theme: Theme) =>
+  createStyles({
+    content: {
+      padding: 15,
+    },
+    linkItemCard :{
+        
+    }
+  }),
+);
