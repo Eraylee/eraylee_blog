fe0697fc586b40fb32895eeeb01936bf10ce4897
eraylee <599023328@qq.com>
2020-06-18 22:45:01 +0800

    修改备案

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box';
 import { useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import { Link } from '../Link';
 import { useStyles } from './style';
 
 export const Footer = () => {
@@ -12,14 +13,14 @@ export const Footer = () => {
     <Box className={classes.footer}>
       <Box className={classes.copyright}>
         <Typography variant='caption' color='primary'>
-          ©eraylee.com. 
+          ©eraylee.com.
         </Typography>
         <Divider orientation='vertical' />
         <Typography variant='caption' color='primary'>
           ❤ Made by ERAYLEE
         </Typography>
       </Box>
-      <Typography variant='caption'>鄂ICP备18019320号-2</Typography>
+      <Link href='http://www.beian.miit.gov.cn'>鄂ICP备18019320号-2</Link>
     </Box>
   );
 };
